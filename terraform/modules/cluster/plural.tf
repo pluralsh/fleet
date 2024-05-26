348204684de6ff9c9daf5978a78baf22036447e5
michaeljguarino <mguarino46@gmail.com>
2024-05-26 19:48:57 -0400

    fix buggy plural cluster setup

--- terraform/modules/cluster/plural.tf
+++ terraform/modules/cluster/plural.tf
@@ -1,10 +1,12 @@
+data "google_client_config" "default" {}
+
 resource "plural_cluster" "this" {
     handle   = var.cluster_name
     name     = var.cluster_name
 
     kubeconfig = {
       host = module.gke.cluster.endpoint
-      cluster_ca_certificate = base64decode(module.mgmt.cluster.ca_certificate)
+      cluster_ca_certificate = base64decode(module.gke.cluster.ca_certificate)
       token = data.google_client_config.default.access_token
     }
 }
