acfdd3e09d60a1c60a71b02ffd3581e6f60d3dff
michaeljguarino <mguarino46@gmail.com>
2024-05-26 19:52:30 -0400

    fix kubeconfig setup

--- terraform/modules/cluster/plural.tf
+++ terraform/modules/cluster/plural.tf
@@ -5,8 +5,8 @@ resource "plural_cluster" "this" {
     name     = var.cluster_name
 
     kubeconfig = {
-      host = module.gke.cluster.endpoint
-      cluster_ca_certificate = base64decode(module.gke.cluster.ca_certificate)
+      host = "https://${module.gke.endpoint}"
+      cluster_ca_certificate = base64decode(module.gke.ca_certificate)
       token = data.google_client_config.default.access_token
     }
 }
