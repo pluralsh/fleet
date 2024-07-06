3645db6b52ae02ea76c1fb6cee382e0ae9d2aeed
michaeljguarino <mguarino46@gmail.com>
2024-07-05 21:04:53 -0400

    fix metadata uid fetch

--- terraform/gke-cluster/locals.tf
+++ terraform/gke-cluster/locals.tf
@@ -3,5 +3,5 @@ locals {
   # db_created = var.create_db ? module.pg.0.google_sql_user.default[0] : {}
   network = "${var.fleet}-${var.cluster}-network"
   subnetwork = "${var.fleet}-${var.cluster}-subnetwork"
-  installable = var.enable_stacks ? module.console-workload-identity.k8s_service_account_name : kubernetes_namespace.plural.uid
+  installable = var.enable_stacks ? module.console-workload-identity.k8s_service_account_name : kubernetes_namespace.plural.metadata[0].uid
 }
