766714a89391032792db719d56c2aea289289b29
michaeljguarino <mjg@plural.sh>
2025-02-10 17:26:18 -0500

    fix existing k8s sa setup

--- terraform/gke-cluster/iam.tf
+++ terraform/gke-cluster/iam.tf
@@ -1,20 +1,10 @@
-resource "kubernetes_namespace" "plural" {
-  count = var.enable_stacks ? 1 : 0
-  metadata {
-    name = "plrl-deploy-operator"
-    labels = {
-      "app.kubernetes.io/managed-by" = "plural"
-    }
-  }
-}
-
 module "console-workload-identity" {
   count               = var.enable_stacks ? 1 : 0
   source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
   name                = "${var.cluster}-plrl-stacks"
   namespace           = "plrl-deploy-operator"
   project_id          = var.project_id
-  use_existing_k8s_sa = false
+  use_existing_k8s_sa = true
   annotate_k8s_sa     = false
   k8s_sa_name         = "stacks"
   roles               = ["roles/owner", "roles/storage.admin"]
@@ -27,7 +17,7 @@ module "externaldns_workload_identity" {
   name                = "${var.cluster}-externaldns"
   namespace           = "external-dns"
   project_id          = var.project_id
-  use_existing_k8s_sa = false
+  use_existing_k8s_sa = true
   annotate_k8s_sa     = false
   k8s_sa_name         = "external-dns"
   roles               = ["roles/dns.admin"]
