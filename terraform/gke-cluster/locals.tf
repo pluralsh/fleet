b82019a6d203bd916e1fe9fa2a20b04126e66b72
michaeljguarino <mjg@plural.sh>
2025-03-19 22:30:43 -0400

    fix vsn math

--- terraform/gke-cluster/locals.tf
+++ terraform/gke-cluster/locals.tf
@@ -5,7 +5,7 @@ locals {
   subnetwork = "${var.fleet}-${var.cluster}-subnetwork"
   installable = var.enable_stacks ? module.console-workload-identity[0].k8s_service_account_name : "installable"
   split_vsn = [ for i in split(".", var.next_kubernetes_version): tonumber(i) ]
-  vsn_even = (tonumber(local.split_vsn[0]) * 100 + tonumber(local.split_vsn[1]) % 2) == 0
+  vsn_even = ((tonumber(local.split_vsn[0]) * 100 + tonumber(local.split_vsn[1])) % 2) == 0
   active_node_group = local.vsn_even ? "blue" : "green"
   drain_node_group = local.vsn_even ? "green" : "blue"
 }
